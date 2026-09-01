4632c57220067d6d7bb3c1db5472180042f1ed89
camila <mmontesinoscr@gmail,com>
2026-08-31 19:28:32 -0700

    Se agrego la clase socio

---
+++ src/model/Socio.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+class Socio:
+
+    def __init__(self, nombre:str, id_sucursal_base:int, activo:bool = True):
+        self.nombre = nombre
+        self.id_sucursal_base = id_sucursal_base
+        self.activo = activo
+
+    def guardar_en_bd(self, conexion:sqlite3.Connection) -> None:
+        cursor = conexion.cursor()
+        query = "INSERT INTO Socio (nombre, id_sucursal_base) VALUES (?, ?)"
+        cursor.execute(query, (self.nombre, self.id_sucursal_base))
+        conexion.commit()
